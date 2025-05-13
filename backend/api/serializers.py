520c5e70ed9895d5ab427f69e7927fb28b4358ef
4MaticAirlines <9509119911s@mail.ru>
2025-05-14 00:04:33 +0300

    end

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,14 @@
+"""TaskSerializer."""
 from rest_framework import serializers
 
 from .models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    """TaskSerializer."""
+
     class Meta:
+        """TaskSerializer."""
+
         model = Task
         fields = ('id', 'title', 'description', 'completed')
